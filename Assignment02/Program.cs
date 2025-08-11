beda35fc6b532520c7ceb9a6d6f64eda12e733b2
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-08-11 04:42:17 +0300

    Q02

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -4,68 +4,118 @@
     {
         static void Main(string[] args)
         {
+            #region Q01
             /* 1. Given an array  consists of  numbers with size N and number of queries,
-             * in each query you will be given an integer X, and you should print how many numbers in array that is greater than  X.
-             * Ex:
-             * Input
-             * 3 3 //Size of array , number of queries
-             * 11 5 3 //Array
-             * 1 //Query1
-             * 5 //Query2
-             * 13 //Query 3
-             * Output
-             * 3 //11,5,3
-             * 1 //11
-             * 0
-             */
+                * in each query you will be given an integer X, and you should print how many numbers in array that is greater than  X.
+                * Ex:
+                * Input
+                * 3 3 //Size of array , number of queries
+                * 11 5 3 //Array
+                * 1 //Query1
+                * 5 //Query2
+                * 13 //Query 3
+                * Output
+                * 3 //11,5,3
+                * 1 //11
+                * 0
+                */
+
+            //int[] arr;
+            //int N, Q, X, countNumbers;
+
+
+            //do
+            //{
+            //    Console.Write("Enter the size of the array N: "); 
+            //} while (!int.TryParse(Console.ReadLine(), out N));
+
+            //arr = new int[N];
+
+
+            //do
+            //{
+            //    Console.Write("Enter the Number of queries : ");
+            //} while (!int.TryParse(Console.ReadLine(), out Q));
+
+            ////Enter the value of the array
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    do
+            //    {
+            //        Console.Write($"Enter the the value of arr[{i}]: ");    
+            //    } while (!int.TryParse(Console.ReadLine(), out arr[i]));
+            //}
+
+            ////Queries
+            //for (int i = 0; i < Q; i++)
+            //{
+            //    countNumbers = 0;
+
+            //    do
+            //    {
+            //        Console.Write("Enter the value of X: "); 
+            //    } while (!int.TryParse(Console.ReadLine(), out X));
+
+            //    for (int j = 0; j < arr.Length; j++)
+            //    {
+
+            //        if (arr[j] > X)
+            //        {
+            //            countNumbers++;
+            //        }
+            //    }
+
+            //    Console.WriteLine(countNumbers);
+            //} 
+            #endregion
+
+            #region Q02
+            /* 2. Given a number N and an array of N numbers. Determine if it's palindrome or not.
+                 * Ex:
+                 * Input:
+                 * 5
+                 * 1 3 2 3 1
+                 * Output:
+                 * YES
+                 */
 
             int[] arr;
-            int N, Q, X, countNumbers;
-            
+            int N;
+            bool ispalindrome = true;
 
             do
             {
-                Console.Write("Enter the size of the array N: "); 
+                Console.Write("Enter the size of the array N: ");
             } while (!int.TryParse(Console.ReadLine(), out N));
 
             arr = new int[N];
 
-
-            do
-            {
-                Console.Write("Enter the Number of queries : ");
-            } while (!int.TryParse(Console.ReadLine(), out Q));
-
-            //Enter the value of the array
+            //Enter the values of the array
             for (int i = 0; i < arr.Length; i++)
             {
                 do
                 {
-                    Console.Write($"Enter the the value of arr[{i}]: ");    
+                    Console.Write($"Enter the the value of arr[{i}]: ");
                 } while (!int.TryParse(Console.ReadLine(), out arr[i]));
             }
 
-            //Queries
-            for (int i = 0; i < Q; i++)
+            for (int i = 0; i < arr?.Length / 2; i++)
             {
-                countNumbers = 0;
-
-                do
+                if (arr[i] != arr[arr.Length - 1 - i])
                 {
-                    Console.Write("Enter the value of X: "); 
-                } while (!int.TryParse(Console.ReadLine(), out X));
-
-                for (int j = 0; j < arr.Length; j++)
-                {
-                   
-                    if (arr[j] > X)
-                    {
-                        countNumbers++;
-                    }
+                    ispalindrome = false;
+                    break;
                 }
-
-                Console.WriteLine(countNumbers);
             }
+
+            if (ispalindrome)
+                Console.WriteLine("YES");
+
+            else
+                Console.WriteLine("NO"); 
+            #endregion
+
+
 
 
 
